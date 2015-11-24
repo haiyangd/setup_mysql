78b4367b8378b9a284a17b70094fd17d8f6fe4de
karia <karia@animatelab.com>
2015-11-24 02:27:39 +0000

    first commit

---
+++ setup_mysql.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+pkgs=(
+        'libncursesw5-dev'
+        'mysql-client'
+        'cmake'
+        'libnagios-plugin-perl'
+        'g++'
+        'libncurses5-dev'
+        'bison'
+        )
+
+mysql_dir_name='mysql-5.7.9'
+mysql_dl_file=$mysql_dir_name'.tar.gz'
+mysql_dl_url='http://dev.mysql.com/get/Downloads/MySQL-5.5/'$mysql_dl_file'/from/http://ftp.iij.ad.jp/pub/db/mysql/'
+mysql_src_path='/usr/local/src'
+mysql_install_path='/usr/local/mysql'
+
+db_user=''
+db_name=''
+
+if [ "$#" -eq "0" ];then
+  echo "usage: $0 --init"
+  exit 0
+fi
+
+if [ "$1" = '--init' ]; then
+        for (( i=0; i<${#pkgs[@]}; i++ ))
+        do
+          apt-get -y install ${pkgs[$i]}
+        done
+
+        # ここにmy.cnfがあると設定がうまくいかないので削除
+        rm /etc/mysql/my.cnf
+
+        wget $mysql_dl_url -O $mysql_src_path/$mysql_dl_file
+  cd $mysql_src_path
+        tar xfz $mysql_src_path/$mysql_dl_file
+        cd $mysql_dir_name
+        cmake -DCMAKE_INSTALL_PREFIX=$mysql_install_path -DDEFAULT_CHARSET=utf8 -DDEFAULT_COLLATION=utf8_general_ci -DWITH_INNOBASE_STORAGE_ENGINE=1 -DDOWNLOAD_BOOST=1 -DWITH_BOOST=/tmp/boost
+        make
+        make install || echo "Install failed!!" && exit 1
+
+  # add unix user
+        useradd mysql -s /bin/false
+        chown -R mysql:mysql $mysql_install_path
+
+        chmod 744 /etc/init.d/mysqld
+        $mysql_install_path/bin/mysqld --datadir=/usr/local/mysql/data --basedir=/usr/local/mysql --user=mysql --log-error-verbosity=3 --initialize-insecure
+
+        # 自動起動登録
+        systemctl enable mysql
+        systemctl start mysql
+
+fi
